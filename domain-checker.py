f095db86da89c1d82240699735a564ef53e79a9e
Matthew Mangus <103114956+mmangus1@users.noreply.github.com>
2022-10-20 05:48:09 -0400

    Update domain-checker.py
    Added some better error handling.

--- domain-checker.py
+++ domain-checker.py
@@ -11,29 +11,41 @@ domains = []
 
 
 def getDomainList():
-    with open('domains.txt','r+') as f:
+    textfile = input("Enter file with a list of domains:")
+    with open(textfile,'r+') as f:
         for domainName in f.read().splitlines():
             domains.append(domainName)
+    return domains
 
 def runAvailabilityCheck():
     for dom in domains:
-        if dom is not None and dom != '':
+         if dom is not None and dom != '':
             try:
                 details = whois.whois(dom)
                 if "contacts" in details.text or "registrant" in details.text:
                     avail.append('False')
                 else:
-                    avail.append('True')
+                     avail.append('True')
             except (whois.parser.PywhoisError):
                 avail.append('True')
+    return avail
 
 def runOutputCSV():
-    with open('persons.csv', 'w', newline='') as f:
+    with open('DomainAvail.csv', 'w', newline='') as f:
         filewriter = csv.writer(f, delimiter=',', quoting=csv.QUOTE_NONE, skipinitialspace=True)
         filewriter.writerow(['Domain Name', 'Available'])
         filewriter.writerows(zip(domains,avail))
 
 if __name__ == "__main__":
-    getDomainList()
-    runAvailabilityCheck()
-    runOutputCSV()
+    try:
+        getDomainList()
+        runAvailabilityCheck()
+        runOutputCSV()
+    except TypeError as e:
+        print(e)
+    except UnicodeDecodeError as e: 
+        print(e)
+    except FileNotFoundError as e:
+        print(e)
+    except:
+        raise
